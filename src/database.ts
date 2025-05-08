ccda9badafd4e60ce22e17a7e7d399926056c9a4
David <davidhastt@gmail.com>
2025-05-08 11:12:21 -0600

    Mejore el archvo database.ts, vinculandolo con el archivo .env

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,15 @@
 import {createPool} from 'mysql2/promise'
+import dotenv from 'dotenv';
+
 
 export async function connect(){
+	dotenv.config()
+
     const connection= await createPool({
 	host: 'localhost',
-	user: 'root',
-	database: 'ogal',
+	user:  process.env.DB_USER,
+	database: process.env.DATABASE,
+	password: process.env.DB_PASSWORD,
 	connectionLimit: 10        
     });
 	return connection;
