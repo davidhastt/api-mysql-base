1bdfaf207bd41726749bc82c317a658964a35a46
David <davidhastt@gmail.com>
2025-05-08 13:28:30 -0600

    Ya estan los endpoint de personas protegida con JWT

--- src/routes/personasRoutes.ts
+++ src/routes/personasRoutes.ts
@@ -1,16 +1,16 @@
 import {Router} from 'express';
 import {login, newUser, personasInfo, getPersonas, getPersona, updatePersona, deletePersona} from '../controllers/persona.controller';
-
+import { authMiddleware } from '../middlewares/auth.middlewares';
 const usersRoutes = Router();
 
 
 
 usersRoutes.get('/personas', personasInfo);
 usersRoutes.post('/personas/nueva', newUser);
-usersRoutes.get('/personas/all', getPersonas);
-usersRoutes.get('/personas/:id_persona', getPersona);
-usersRoutes.put('/personas/:id_persona', updatePersona);
-usersRoutes.delete('/personas/:id_persona', deletePersona);
+usersRoutes.get('/personas/all', authMiddleware, getPersonas);
+usersRoutes.get('/personas/:id_persona', authMiddleware, getPersona);
+usersRoutes.put('/personas/:id_persona', authMiddleware, updatePersona);
+usersRoutes.delete('/personas/:id_persona', authMiddleware, deletePersona);
 
 
 usersRoutes.post('/personas/login', login);
